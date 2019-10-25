5517edd02a6cb0687efc11f5fc96f2d9146df574
JU5TU5d <vladislav261100@gmail.com>
2019-10-25 11:18:50 +0300

    Updates

---
+++ precache-manifest.def014c6281966c3cff476fb93d51041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b604289aea55691f77c6dcc4aa10fca1",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "46297a900b66618c058e",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.b18376dc.chunk.css"
+  },
+  {
+    "revision": "e1f244e7253003a9ff5e",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.f5e90879.chunk.js"
+  },
+  {
+    "revision": "46297a900b66618c058e",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.f280d6d1.chunk.js"
+  },
+  {
+    "revision": "549cb78f4abd9d607011",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0cda3cff.js"
+  }
+]);
